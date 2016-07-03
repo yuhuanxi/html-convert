a8b0f3cac40bfa6578f6f5e9dfe9f199ddb77df1
iepngs <iepngs@aliyun.com>
2016-07-03 18:34:12 +0800

    Update convert.js
    1、it seems no needed to use koa application framework!
    2、anyway, by yourself, it's complished. enjoin it.
    3、what's more, fuck your sleep early to be game or America movie or else!
    4、please translate this into correct: '嘘寒问暖，不如打笔巨款'.

--- app/convert.js
+++ app/convert.js
@@ -10,40 +10,65 @@ var router = require('koa-router')();
 var uuid = require('node-uuid');
 var path = require('path');
 
-const Pageres = require('pageres');
+var Pageres = require('pageres');
 
-router
-    .get('/convert', function*() {
-        var qu = this.request.query;
-        var url = qu.url;
-        var size = qu.size;
+router.get('/convert/:url/:size', function* (ctx, next) {
+        var remoteUrl = decodeURIComponent(this.params.url);
+        var size = this.params.size;
         var imgName = uuid.v4().split('-')[0];
 
-        var workDir = path.resolve('./');
-        console.log(workDir);
-
-        // 组合完整的文件路径:当前目录+'pub';
-        var filePath = path.join(workDir, 'img');
-
+        var filePath = path.join(__dirname, 'img');
+		console.log(remoteUrl, size, filePath, imgName);
+		
+        //TODO You can not handl the time when the window onload finished'
         new Pageres({delay: 2, filename: imgName})
-            .src(url, [size])
+            .src(remoteUrl, [size])
             .dest(filePath)
             .run()
-            .then(()=> {
+            .then(function() {
                 console.log('done');
             });
-        this.body = 'Hello World';
+        
+        var fileFullPath = path.join(filePath, imgName);
+	  	//pagers formart png & jpg default png, it seems not support pdf. 
+	  	//sad story! so you have to do something else...
+	  	var mimetype = 'image/png' || 'application/pdf';        
+
+		this.set('Content-disposition', 'attachment; filename=' + imgName);
+	  	this.set('Content-type', mimetype);
+		
+		var fs = require('fs');
+	  	var filestream = fs.createReadStream(fileFullPath);
+	  	this.body = yield new Promise(function(resolve,reject){  
+	        fs.readFile(fileFullPath, function(err,data){  
+	            if(err){  
+	                reject(err);//文件存在返回true  
+	            }else{  
+	                resolve(data);//文件不存在，这里会抛出异常  
+	            }
+	        });  
+	    }).then(function(data){
+	            console.log(data);  
+	            return data;  
+		},function(err){  
+	            console.log(err);  
+	            return err;  
+	     });
     })
 
-app
-    .use(router.routes())
-    .use(router.allowedMethods());
 
-console.log('服务已经启动');
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+console.log('message: ' + "PhantomJS, which is used for generating the screenshots, "+
+"is installed automagically, but in some rare cases it might fail to and you'll get an Error: "+
+"spawn EACCES error. Download(url:http://phantomjs.org/download.html) "+
+"PhantomJS manually and reinstall pageres if that happens.");
 
 console.log('请在地址栏输入 ' +
-    'http://localhost:3000/convert?' +
-    'url=http://www.baidu.com' +
-    '&size=1920x1080 ' + '进行测试');
+    'http://127.0.0.1:3000/convert/' + 
+    encodeURIComponent('http://www.baidu.com?a=1&b=2') +
+    '/1920x1080 ' + '进行测试');
 
 app.listen(3000);
